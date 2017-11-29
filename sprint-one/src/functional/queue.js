29a927d987950586a65cade5608735bc9675c1ba
Derrick Cross <decross1@me.com>
2017-11-29 13:20:31 -0800

    Implement functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
+  var end = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[start] = value;
+    start++;
   };
 
   someInstance.dequeue = function() {
+    if (end < start) {
+      var val = storage[end];
+      delete storage[end];
+      end++;
+      return val;
+    }
   };
 
   someInstance.size = function() {
+    return start - end;
   };
 
   return someInstance;
