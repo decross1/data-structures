271f8a7f829ac8987379708bd2b6ee8906a833cc
decross1 <decross1@me.com>
2017-11-29 15:44:53 -0800

    functional-shared stack working

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,45 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create instance object
+  var instanceObject = {};
+
+  // Create size variable
+  instanceObject.counter = 0;
+
+  // Create storage object
+  instanceObject.storage = {};
+
+  // Extend instance with stackMethods
+  _.extend(instanceObject, stackMethods);
+
+  return instanceObject;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // Push Method
+  push: function(value) {
+    // add new value into storeage at key size
+    this.storage[this.counter] = value;
+    // increment
+    this.counter += 1; 
+  },
+
+  // Pop Method
+  pop: function() {
+    if (this.counter === 0) { 
+      return undefined;
+    } else if (this.counter > 0) {
+      var poppedObject = this.storage[this.counter - 1];
+      delete this.storage[this.counter - 1];
+      this.counter -= 1; 
+      return poppedObject;
+    }
+  },
+
+
+  // Size Method
+  size: function() {
+    return this.counter;
+  }
+};
 
 
