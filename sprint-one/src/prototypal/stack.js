10f3c8920f9a29464b78216b5e0d4f03146cc37c
decross1 <decross1@me.com>
2017-11-29 16:42:49 -0800

    queue psueduclassical and all stacks complete

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var someInstance = Object.create(stackMethods);
+  someInstance.counter = 0;
+  someInstance.storage = {};
+  
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter += 1;
+  }, 
+
+  pop: function() {
+    if (this.counter === 0) {
+      return undefined;
+    } else if (this.counter > 0) {
+      var poppedValue = this.storage[this.counter - 1];
+      delete this.storage[this.counter - 1];
+      this.counter -= 1;
+      return poppedValue;
+    }
+  }, 
+
+  size: function() {
+    return this.counter;
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.queueLen = 0;
+  this.tailIndex = 0;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.tailIndex] = value;
+  this.tailIndex += 1; 
+  this.queueLen += 1;  
+};
+
+Queue.prototype.dequeue = function () {
+  if (this.queueLen === 0) {
+    return undefined;
+  } else if (this.queueLen > 0) {
+    var removedValue = this.storage[this.tailIndex - this.queueLen];
+    delete this.storage[this.tailIndex - this.queueLen];
+    this.queueLen -= 1;
+    return removedValue;
+  }
+};
+
+Queue.prototype.size = function () {
+  return this.queueLen;
+};
 
