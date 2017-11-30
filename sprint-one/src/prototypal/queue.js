d0c4d4fb68771d0a80e96f305b48b0c7fa7806ca
decross1 <decross1@me.com>
2017-11-29 20:28:58 -0800

    queue for prototypal is working

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods);
+  someInstance.headIndex = 0;
+  someInstance.tailIndex = 0;
+  someInstance.storage = {}; 
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.tailIndex] = value;
+    this.tailIndex += 1; 
+  },
+  dequeue: function() {
+    debugger;   
+    if (this.size() === 0) {
+      return undefined;
+    } else if (this.size() > 0) {
+      
+      var removedValue = this.storage[this.headIndex];
+      delete this.storage[this.headIndex];
+      this.headIndex += 1;
+      return removedValue;
+    }
+  },
+  size: function() {
+    return this.tailIndex - this.headIndex;
+  }
+};
 
 
