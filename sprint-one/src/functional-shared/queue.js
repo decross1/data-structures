811506b5f5d9cd577238cd8a2b0c404e81ee6309
decross1 <decross1@me.com>
2017-11-29 20:43:35 -0800

    Final tests all working

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.headIndex = 0;
+  someInstance.tailIndex = 0;
+  someInstance.storage = {};
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    this.storage[this.tailIndex] = value;
+    this.tailIndex += 1;
+  },
+  dequeue: function (value) {
+    if (this.size() === 0) {
+      return undefined;
+    } else if (this.size() > 0) {
+      var removedElement = this.storage[this.headIndex];
+      delete this.storage[this.headIndex];
+      this.headIndex += 1;
+      return removedElement;
+    }
+  },
+  size: function (value) {
+    return this.tailIndex - this.headIndex;
+  } 
+  
+};
 
 
