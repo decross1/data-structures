2c4c9544d41943c8b03cddb53ec35f8e48c2ec86
decross1 <decross1@me.com>
2017-12-01 18:35:35 -0800

    binary search tree passing contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,78 @@
 var BinarySearchTree = function(value) {
+  var thisInstance = Object.create(BinarySearchTree.prototype);
+  thisInstance.value = value;
+  thisInstance.left = null;
+  thisInstance.right = null;
+  return thisInstance;
 };
 
+BinarySearchTree.prototype.contains = function(target) {
+  // Check to see if this.value equals our target
+  if (this.value === target) {
+  // if it is our target
+    // return true
+    return true;
+  } else if (target > this.value && this.right) {
+  // else if (this.value > target) and this.right is not null 
+    //  if contains(this.right) is true, return true 
+    if (this.contains.call(this.right, target)) {
+      return true;
+    }
+  } else if (target < this.value && this.left) {
+  // else if (this.value < target) and this.left is not null 
+    //  if contains(this.left) is true, return true 
+    if (this.contains.call(this.left, target)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+BinarySearchTree.prototype.insert = function(insertValue) {
+  // create function called walkAndAdd(tree)
+  
+  var walkAndAdd = function(tree) {
+    // compare insertValue to this.value
+    // if this.value > insertValue
+    if (tree.value < insertValue) {
+      // is this.right not null
+      if (!tree.right) {
+        // this.right = newnode(insertValue)
+        tree.right = Tree(insertValue);
+      // else
+      } else {
+        // walkAndAdd(this.right)
+        walkAndAdd(tree.right);
+      }
+    } else if (tree.value > insertValue) {
+      // is this.right not null
+      if (!tree.left) {
+        // this.left = newnode(insertValue)
+        tree.left = Tree(insertValue);
+      // else
+      } else {
+        // walkAndAdd(this.left)
+        walkAndAdd(tree.left);
+      }
+    }
+  };
+
+  walkAndAdd(this);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+
+};
+
+BinarySearchTree.prototype.makeNewTreeNode = function(value) {
+  return {
+    value: value,
+    left: null,
+    right: null
+  };
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,113 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // create array insertArray [k , v];
+  var insertArray = [k, v];
+  // check items at index and store in var itemsAtIndex
+  var numOfItems = this.itemsAtIndex(index);
+  // if items = 0
+//  debugger;
+  if (numOfItems === 0) { 
+    // make this._storage[index] = insertArray
+    this._storage[index] = insertArray; 
+  // else if items = 1
+  } else if (numOfItems === 1) {
+    // var old = this._storage[index];
+    var existing = this._storage[index];
+    // overwrite this._storage[index] = [old, insertArray]
+    // if it's a new unique key:
+    if (this._storage[index][0] !== k) {
+      // else
+      this._storage[index] = [existing, insertArray];
+    } else {
+      //update the value at key bob
+      this._storage[index][1] = v; 
+    }
+    // else if items >= 2
+  } else if (numOfItems >= 2) {
+    // push to this._storage
+    
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+        break;
+      }
+      // On the last loop of the for loop, if we haven't
+      // found an existing element with the same key
+      // push that element in.
+      if (i === this._storage[index][length - 1]) {
+        this._storage[index].push(insertArray);   
+      }    
+    } 
+  } 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  console.log(this._storage);
+  // check number of items at Index
+//  debugger;
+  var numOfItems = this.itemsAtIndex(index);
+  var arrayAtIndex = this._storage[index];
+  // If there are 0 items
+  if (numOfItems === 0) {
+    // return undefined 
+    return undefined;
+  } else if (numOfItems === 1) {
+  // If there is exactly one item
+    // return the v (this._storage[index][1])
+    return arrayAtIndex[1]; 
+  } else {
+  // If there is more than one
+    // iterate over the array at the index
+    for (var i = 0; i < arrayAtIndex.length; i++) {
+    // for each array, check if 1st at index 0 element is equal to k
+      // if it is, return 2nd element
+      if (arrayAtIndex[i][0] === k) {
+        return arrayAtIndex[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Check for how many items  
+  var numOfItems = this.itemsAtIndex(index);
+  // else if item check is 1
+  if (numOfItems === 1) {
+    this._storage[index] = undefined;
+  // else if item greater or equal than two 2
+  } else if (numOfItems > 1) {
+    // loop through the array at the matching index.
+    var arrayAtIndex = this._storage[index];
+    for (var i = 0; i < arrayAtIndex.length; i++) {
+    // if item is found
+      if (arrayAtIndex[i][0] === k) {
+      // splice out that item at item's index
+        arrayAtIndex.splice(i, 1);
+      // if item check 2
+        if (numOfItems === 2) {
+          _.flatten(arrayAtIndex, true);
+        // also flatten the results
+        }
+      }
+    }
+  }
 };
 
+HashTable.prototype.itemsAtIndex = function(index) {
+  var arrayAtIndex = this._storage[index];
 
+  // If arrayAtIndex[0] is not an array
+  if (arrayAtIndex === undefined) {
+    return 0;
+  } else if (!Array.isArray(arrayAtIndex[0])) {
+    return 1;
+  } else if (Array.isArray(arrayAtIndex[0])) {
+    return arrayAtIndex.length;
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
